fd5fea0f3f93dc859f26c3903f91f64e89711d0f
GregoryVinicius <gregoryviniciuscla@gmail.com>
2023-05-24 21:00:14 -0300

    Criando personagem

--- src/br/ifpr/jogo/modelo/Fase.java
+++ src/br/ifpr/jogo/modelo/Fase.java
@@ -1,14 +1,29 @@
 package br.ifpr.jogo.modelo;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
-public class Fase {
+public class Fase extends JPanel{
     private Image fundo;
+    private Image personagem;
+    
 
     public Fase(){
-        ImageIcon carregando = new ImageIcon("", null);
+        ImageIcon carregando = new ImageIcon("recursos\\imagem-fundo.jpg");
+        this.fundo = carregando.getImage();
+        
+        this.personagem = new Personagem();
+        this.personagem.carregar();
+    }
+
+    public void paint(Graphics g){
+        Graphics2D graficos = (Graphics2D) g;
+        graficos.drawImage(this.fundo , 0, 0, null);
+
     }
 
 }
--- src/br/ifpr/jogo/principal/Principal.java
+++ src/br/ifpr/jogo/principal/Principal.java
@@ -2,15 +2,19 @@ package br.ifpr.jogo.principal;
 
 import javax.swing.JFrame;
 
+import br.ifpr.jogo.modelo.Fase;
+
 public class Principal extends JFrame{
     public Principal(){
+        Fase fase = new Fase();
+        super.add(fase);
         setVisible(true);
-        setSize(614, 335);
+        setSize(1920, 1080);
         setTitle("Jogo bom demaizi");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     public static void main(String[] args){
-        Principal principal = new Principal();
+        Principal principal = new Principal(); 
     }
 }
