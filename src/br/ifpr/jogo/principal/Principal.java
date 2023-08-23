14be7617ae9a82e04446375e0fdb0ad48440ae83
AlunoIfpr <alunoifpr.gmail.com>
2023-08-23 19:50:13 -0300

    Criando classe Asteroid

--- src/br/ifpr/jogo/principal/Principal.java
+++ src/br/ifpr/jogo/principal/Principal.java
@@ -6,11 +6,14 @@ import br.ifpr.jogo.modelo.Fase;
 import br.ifpr.jogo.modelo.FaseUm;
 
 public class Principal extends JFrame{
+    public static int ALTURA_DA_JANELA = 1080; 
+    public static int LARGURA_DA_JANELA = 1920;
+
     public Principal(){
         Fase fase = new FaseUm();
         super.add(fase);
         setVisible(true);
-        setSize(1920, 1080);
+        setSize(LARGURA_DA_JANELA, ALTURA_DA_JANELA);
         setTitle("Jogo Bonzão");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
