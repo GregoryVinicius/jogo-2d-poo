7187dcfd770131097bda7ce07390c7956da5542d
GregoryVinicius <gregoryviniciuscla@gmail.com>
2023-11-21 10:19:45 -0300

    problema com o banco de dados

--- src/main/java/br/ifpr/jogo/dao/JogadorDao.java
+++ src/main/java/br/ifpr/jogo/dao/JogadorDao.java
@@ -5,12 +5,8 @@ import br.ifpr.jogo.modelo.Jogador;
 
 public interface JogadorDao {
     public List<Jogador> buscarTodos();
-
     public Jogador buscarPorId(Integer id);
-
     public void atualizar(Jogador jogador);
-
     public void excluir(Jogador jogador);
-
     public void inserir(Jogador jogador);
 }
